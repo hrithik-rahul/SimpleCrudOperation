5c9ca43dbb96da8aeaeeaacfe8ed0a2abace42a5
hrithik-rahul <92098271+hrithik-rahul@users.noreply.github.com>
2023-09-13 21:58:49 +0530

    Delete CrudOperation directory

--- CrudOperation/src/main/java/com/rahul/CrudOperation/repositary/SubjectRepositary.java
+++
@@ -1,9 +0,0 @@
-package com.rahul.CrudOperation.repositary;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.rahul.CrudOperation.bean.Subject;
-
-public interface SubjectRepositary extends CrudRepository<Subject,String> {
-
-}
